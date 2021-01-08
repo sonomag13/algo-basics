d6b4f912898f4cca923be18d4c2c2d1620563f00
Lu Gao <sonomag13@gmail.com>
2021-01-07 20:41:53 -0800

    refactor the sparse vector inner product

--- sparse-vector/main.cpp
+++ sparse-vector/main.cpp
@@ -1,50 +1,56 @@
 #include <iostream>
 #include <unordered_map>
 
-using namespace std;
+using namespace std; 
 
 class SparseVector {
 public: 
-  SparseVector(unordered_map<int, double> mapIdxVal) : _mapIdxVal(mapIdxVal) { }
+  SparseVector(unordered_map<size_t, double> mapIdxVal) : _mapIdxVal(mapIdxVal) { }
     
   ~SparseVector() = default; 
   
-  double innerProduct(const SparseVector& vector2) const {    
-      double sum;     
+  double innerProduct(const SparseVector& sparseVectorIn) const {    
+      // calcualte the inner product between two sparse vectors
+      double sum; 
       for(auto& ele : _mapIdxVal) {        
-        sum += ele.second * vector2.getVal(ele.first);         
+        /**
+         * traverse the non-trivial values in the hash map, and multiple with the
+         * corresponding value in the input sparse vector
+         **/
+        sum += ele.second * sparseVectorIn.getVal(ele.first);
       }      
       return sum; 
   }
   
-  double getVal(int key) const {
-    if (_mapIdxVal.find(key) != _mapIdxVal.end()) {
-      return _mapIdxVal.at(key); 
-    }
-    else {
-      return 0.0; 
-    }    
+  double getVal(size_t key) const {
+    /**
+     * return the value if the key can be found in the map, i.e. the index 
+     * points to a non-trivial value; otherwise, return 0.0
+     **/ 
+    return _mapIdxVal.find(key) != _mapIdxVal.end() ? 
+           _mapIdxVal.at(key) : 0.0; 
   }
   
 private: 
-  unordered_map<int, double> _mapIdxVal; 
+  /**
+   * an unordered map is used to store the values in the parse vector
+   * - key: the index of a non-trivial value in a sparse vector
+   * - value: a non-trival value
+   **/
+  unordered_map<size_t, double> _mapIdxVal; 
   
 };
 
-
-// To execute C++, please define "int main()"
-int main() {
+int main() {  
   
-  unordered_map<int, double> map1{make_pair(0, 1.0), make_pair(1, 1.0), make_pair(4, 3)}; 
-  unordered_map<int, double> map2{make_pair(0, 1.0), make_pair(4, 3)}; 
+  SparseVector sparseVector1(unordered_map<size_t, double> {make_pair(0, 1.0), make_pair(1, 1.0), make_pair(4, 3.0)}); 
+  SparseVector sparseVector2(unordered_map<size_t, double> {make_pair(0, 1.0), make_pair(4, 3.0)}); 
   
-  SparseVector vector1(map1); 
-  SparseVector vector2(map2); 
-  
-  double innerProduct = vector1.innerProduct(vector2); 
+  // the expected value is 10
+  double innerProduct = sparseVector1.innerProduct(sparseVector2); 
   
   cout << "inner product = " << innerProduct << endl;   
   
-  return 0; 
-}
+  return EXIT_SUCCESS; 
 
+}
